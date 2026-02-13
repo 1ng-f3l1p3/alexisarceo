bb313ed9dcc8e92212c648b5ca53cf0242110932
ISC FELIPE CHIM <felipito_chch91@hotmail.com>
2026-02-12 20:53:14 -0600

    deleted config vu3 incompatible

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,1 @@
-import { createApp } from 'vue';
-import MainPage from './components/MainPage.vue';
-
-const app = createApp({});
-
-app.component('main-page', MainPage);
-
-app.mount('#app');
+import './bootstrap';
